10d5a0bb28b7883939131d72fef8ea350be8eb73
stephengaudet <32783698+stephengaudet@users.noreply.github.com>
2024-02-13 13:34:36 -0330

    Delete graf_token.sh
    renamed to init_grafana.sh

--- graf_token.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#can create token only once
-curl -X POST -H "Content-Type: application/json" -d '{"name":"apikeycurl", "role": "Admin"}' http://admin:admin@localhost:3000/api/auth/keys
